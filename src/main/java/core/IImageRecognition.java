2c13d1b4c98ff81412f71afd2c594a44ee47e96b
LiuFangGuo <guoliufang@tigerjoys.com>
2015-10-26 19:02:40 +0800

    创建处理灰度的多种重载方法

--- src/main/java/core/IImageRecognition.java
+++ src/main/java/core/IImageRecognition.java
@@ -8,13 +8,22 @@ import java.util.Map;
  * Created by LiuFangGuo on 10/21/15.
  */
 public interface IImageRecognition {
+    int GRAY_METHOD_AVERAGE = 1;
+    int GRAY_METHOD_LUMINOSITY = 2;
+    int GRAY_METHOD_DESATURATION = 3;
+    int GRAY_METHOD_DECOMPOSITON = 4;
+    int GRAY_METHOD_SINGLE_COLOR = 5;
+
     //预处理方法族群，目前包括isWhite,removeBackgroud
     boolean isWhite(int rgb);
 
     BufferedImage removeBackgroud(String imagePath);
 
+    BufferedImage getGrayScale(String imagePath, int type);
+
     //图像分割方法族群，目前包括splitImage
     List<BufferedImage> splitImage(BufferedImage bufferedImage);
+
     void splitImage(String imagesDirctoryPath);
 
     //训练方法族群，目前包括train
---
+++ src/main/java/three/ThreeTrying.java
@@ -0,0 +1,20 @@
+package three;
+
+import core.AImageRecognition;
+
+import java.awt.image.BufferedImage;
+import java.util.List;
+
+/**
+ * Created by LiuFangGuo on 10/26/15.
+ */
+public class ThreeTrying extends AImageRecognition {
+
+    public ThreeTrying(String configFileName) {
+        super(configFileName);
+    }
+
+    public List<BufferedImage> splitImage(BufferedImage bufferedImage) {
+        return null;
+    }
+}
